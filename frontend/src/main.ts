2dbf6126268e0e63822df42e52a9ce349906bde3
lanyeeee <1210347077@qq.com>
2024-05-26 14:45:45 +0800

    feat: 引入unocss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import {createPinia} from 'pinia'
 import App from './App.vue'
 import './style.css';
+import 'virtual:uno.css'
 
 const pinia = createPinia()
 const app = createApp(App)
