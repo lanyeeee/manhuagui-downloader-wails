4b014e4a8f8dc27a2cb4b5fee5c4eca5023bb89f
lanyeeee <1210347077@qq.com>
2024-05-28 01:04:07 +0800

    refactor: 将是否展示导出树的逻辑从ExportTree组件移到ExportPane

--- frontend/src/components/export/ExportTree.vue
+++ frontend/src/components/export/ExportTree.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import {NTree, TreeInst, TreeOption, TreeOverrideNodeClickBehavior} from "naive-ui"
-import {computed, defineModel, ref, watch} from "vue"
+import {defineModel, ref, watch} from "vue"
 
 
 const exportTreeInst = defineModel<TreeInst | null>("exportTreeInst", {required: true});
@@ -8,8 +8,7 @@ const exportTreeOptions = defineModel<TreeOption[]>("exportTreeOptions", {requir
 const exportDefaultExpandKeys = defineModel<string[]>("exportDefaultExpandKeys", {required: true});
 const exportDefaultCheckedKeys = defineModel<string[]>("exportDefaultCheckedKeys", {required: true});
 
-// TODO: 换成<"empty", "tree">的联合类型
-const showInfo = computed<boolean>(() => exportTreeOptions.value.length === 0)
+
 const treeRef = ref<TreeInst | null>(null)
 watch(treeRef, () => {
   exportTreeInst.value = treeRef.value
@@ -26,10 +25,8 @@ const treeNodeClickBehaviour: TreeOverrideNodeClickBehavior = ({option}) => {
 
 <template>
   <div class="h-full overflow-hidden">
-    <n-result v-if="showInfo" title="缓存目录为空"/>
     <n-tree
         class="text-align-left"
-        v-if="!showInfo"
         virtual-scroll
         block-line
         show-line
